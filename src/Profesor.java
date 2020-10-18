81a97afe705c6ce270a376d57b6391f29a52a812
Melanie <melanie.espinoza@pucp.edu.pe>
2020-10-18 07:57:43 -0500

    Revert "se edito prof, persona, admin"
    This reverts commit a864745d

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,11 +1,3 @@
 public class Profesor {
-    private String curso;
 
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
 }
