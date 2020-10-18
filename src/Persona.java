f00c6778392f2536697f2c0353fa55e2f765d0e8
Melanie <melanie.espinoza@pucp.edu.pe>
2020-10-18 07:53:56 -0500

    Se agrego un atributo a persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Persona {
+public class Persona {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
