a864745df41e5c31c811b01c36f3e4a9052cf03e
Melanie <melanie.espinoza@pucp.edu.pe>
2020-10-18 07:56:42 -0500

    se edito prof, persona, admin

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,3 +1,11 @@
 public class Profesor {
+    private String curso;
 
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
